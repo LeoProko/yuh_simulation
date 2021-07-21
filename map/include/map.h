731063e75eec85cd3dffcf942aa8a63be51acfb9
Leo Proko <prokop4uk.lyonya@yandex.ru>
2021-07-21 12:33:22 +0300

    add executable file & solve incomplit type problem (#5)
    * add executable file & solve incomplit type problem
    
    * [map] replacing size_t with int

--- map/include/map.h
+++ map/include/map.h
@@ -4,4 +4,17 @@
 #include <vector>
 #include <random>
 
-class Map;
+class Map {
+private:
+    std::vector<std::vector<Cell>> map_;
+    int size_;
+    int bots_amount_;
+    int food_amount_;
+
+public:
+    Map(int size, int bots_amount, int food_amount);
+
+    void RespawnFood();
+
+    std::vector<Cell>& operator[](const int i);
+};
---
+++ map/include/position.h
@@ -0,0 +1,8 @@
+struct Position {
+    int x;
+    int y;
+    
+    Position() = delete;
+
+    Position(int first, int second);
+};
