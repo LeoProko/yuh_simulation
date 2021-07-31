cad3d43464be471baba62d140ea47346a8249c74
levelup0 <leo.rybakov@icloud.com>
2021-08-01 00:37:13 +0300

    [move] rewrite move function

--- move/src/move.cpp
+++ move/src/move.cpp
@@ -1,9 +1,10 @@
 #include "move.h"
 
-void move(Bot& bot, Map& map) {
-    if (rand() < bot.intelligence_) {
-        std::vector<Position> best_positions;
-        int max_food = 0;
+void move(Cell& cell, Map& map) {
+    for (int i = 0; i < cell.bot_counter_; ++i) {
+        Bot& bot = cell.bots_.front();
+        std::vector<Position> positions;
+
         int left_border   = std::max(0,          bot.position_.x - 1);
         int right_border  = std::min(map.size(), bot.position_.x + 2);
         int top_border    = std::max(0,          bot.position_.y - 1);
@@ -11,20 +12,21 @@ void move(Bot& bot, Map& map) {
 
         for (int i = left_border; i < right_border; ++i) {
             for (int j = top_border; j < bottom_border; ++i) {
-                if (map[i][j].food_counter_ > max_food) {
-                    max_food = map[i][j].food_counter_;
-                    best_positions.clear();
-                    best_positions.emplace_back(i, j);
-                } else if (map[i][j].food_counter_ == max_food) {
-                    max_food = map[i][j].food_counter_;
-                    best_positions.emplace_back(i, j);
-                }
+                positions.emplace_back(i, j);
             }     
         }
 
-        bot.position_ = best_positions[rand() % best_positions.size()];
-    } else {
-        bot.position_.x += rand() % 3 - 1;
-        bot.position_.y += rand() % 3 - 1;
+        std::sort(
+            positions.begin(), 
+            positions.end(), 
+            [&] (const Position& first, const Position& second) {
+                return map[first].food_counter_ < map[second].food_counter_;
+            }
+        );
+
+        bot.position_ = positions[bot.intelligence_ * positions.size() / 100];
+
+        map[bot.position_].bots_.push_back(bot);
+        cell.bots_.pop_front();
     }
 }
