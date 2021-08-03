c0c67910dd024dd61ac1d31fe89ccfcd0366c35e
neudachina <neudachina@192.168.1.6>
2021-08-03 15:10:36 +0300

    fixed

--- cell/include/cell.h
+++ cell/include/cell.h
@@ -11,16 +11,17 @@ private:
 
     void split_food();
 
-    void add_bot(Bot& bot);
+    int total_coef_ = 0;
 
 public:
     int bot_counter_ = 0;
     int food_counter_ = 0;
-    int total_coef_ = 0;
 
     std::deque<Bot*> bots_;
 
     Cell() = default;
 
     void do_all();
+
+    void add_bot(Bot& bot);
 };
